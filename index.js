c607a3a998591bed38f079bcfcb1ec66f1d8d4f7
aleena <56083273+aleenuh@users.noreply.github.com>
2021-10-01 03:40:51 +0100

    Added index.js to repository
    Contains the main code for TopiaBot

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config(); // initialize dotenv
+const Discord = require('discord.js'); //importing discord.js
+
+const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] }); // create new client (intents req.)
+
+client.on('ready', () => { // when bot is ready do this.. (no parameters)
+    console.log(`${client.user.tag} is online! ✦`);
+});
+
+client.on('message', msg => { // if user posts msg, reply with..
+    if (msg.content === "HI TOPIABOT") {
+        msg.reply('hello! ✦');
+    }
+
+client.login(process.env.CLIENT_TOKEN); // login bot using token (.env file)
