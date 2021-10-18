7b25bbc1419b9d3973bfafd31ce38f1bf44057cb
Olaf <o.olaf@hotmail.com>
2021-10-19 00:23:56 +0200

    First Database connection
    - still need to save bytearray to image and link the image to the embed (probably need database change to know the file extention)

---
+++ MysqlDB.js
@@ -0,0 +1,48 @@
+var mysql = require('mysql2');
+require('dotenv').config(); // initialize dotenv
+
+var con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    database: process.env.DB_DATABASE
+});
+
+module.exports = {
+    TestConnection: function () {
+        return con.connect(function(err) {
+            if (err) {
+                console.log(err.sqlMessage);
+                throw new err;
+            }
+            return con.query("SELECT * FROM DropChance" , function (err, result, fields) {
+                if (err) {
+                    console.log(err.sqlMessage);
+                    throw new err;
+                }
+                console.log(result);
+            });
+        });
+    },
+    GetCard : function (tier, callback) {
+        return this.GetCardFromDatabase(tier, function(result){
+            if(result.length > 0) {
+                return callback(result[index]);
+             }
+        });
+    },
+    GetCardFromDatabase: function (tier, callback) {
+        return con.query("SELECT * FROM PhotoCard WHERE Tier = ?;", [tier],
+        function (err, result, fields) {
+            if (err) {
+                console.log("Error Olaf" + err);
+                throw new err;
+            }
+            if(result.length > 0) {
+                var index = Math.floor(Math.random() * (result.length - 0) + 0);
+                console.log(result[index]);
+                return callback(result[index].CodeName,result[index].Tier,result[index].FileBlob);
+            }
+            return callback("", "No cards found :sob:" , "");
+        });
+    }
+};
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config(); // initialize dotenv
 const Discord = require('discord.js'); //importing discord.js
+const mysqlDB = require('./MysqlDB.js');
+const MessageEmbed = Discord.MessageEmbed;
 
 const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] }); // create new client (intents req.)
 
@@ -7,10 +9,40 @@ client.on('ready', () => { // when bot is ready do this.. (no parameters)
     console.log(`${client.user.tag} is online! ✦`);
 });
 
-client.on('message', msg => { // if user posts msg, reply with..
+client.on('messageCreate', msg => { // if user posts msg, reply with..
     if (msg.content === "HI TOPIABOT") {
         msg.reply('hello! ✦');
     }
+    else if (!isNaN(parseInt(msg.content))) {
+        var tier = parseInt(msg.content);
+        mysqlDB.GetCardFromDatabase(tier, function (codename, tier, blob) {
+            if (typeof tier === 'string' || tier instanceof String) {
+                msg.reply(tier);
+                return;
+            }
+    
+            console.log(codename);
+    
+            var embed = new MessageEmbed()
+                .setTitle(codename)
+                .setDescription("Tier: " + tier);
+            msg.channel.send({ embeds: [embed]});
+        });
+    };
 });
+
+function saveImage(filename, data){
+    var myBuffer = new Buffer.from(data.length);
+    for (var i = 0; i < data.length; i++) {
+        myBuffer[i] = data[i];
+    }
+    fs.writeFile('./'+filename, myBuffer, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved!");
+        }
+    });
+  };
 
 client.login(process.env.CLIENT_TOKEN); // login bot using token (.env file)
